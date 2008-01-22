ceed00475126d543aaaba54c1c99a71cecf7888d
felmer <felmer>
2008-01-22 13:37:39 +0000

    initial data model and inital data
    SVN: 3711

---
+++ cifex/source/sql/generic/001/data-001.sql
@@ -0,0 +1,19 @@
+----------------------------------------------------------------------
+--  Purpose:  Create an initial admin (password: admin) in USERS
+-----------------------------------------------------------------------
+
+insert into users
+(id
+,email
+,user_id
+,encrypted_password
+,is_admin
+,is_permanent)
+values
+(nextval('USER_ID_SEQ')
+,'admin@localhost'
+,'admin'
+,'21232f297a57a5a743894a0e4a801fc3'
+,'T'
+,'T'
+);
