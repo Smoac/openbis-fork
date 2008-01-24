92fbfa8cfe528044bb143e770792782f6761dead
izabel <izabel>
2008-01-24 10:50:56 +0000

    Submitted for team work
    SVN: 3771

---
+++ cifex/source/java/ch/systemsx/cisd/cifex/server/business/dto/FileDTO.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2008 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.cifex.server.business.dto;
+
+import java.util.Date;
+
+/**
+ * @author Izabela Adamczyk
+ */
+public class FileDTO extends ID
+{
+    String name;
+
+    String path;
+
+    UserDTO registerer;
+
+    Date registrationDate;
+
+    Date expirationDate;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getPath()
+    {
+        return path;
+    }
+
+    public void setPath(String path)
+    {
+        this.path = path;
+    }
+
+    public UserDTO getRegisterer()
+    {
+        return registerer;
+    }
+
+    public void setRegisterer(UserDTO registerer)
+    {
+        this.registerer = registerer;
+    }
+
+    public Date getRegistrationDate()
+    {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate)
+    {
+        this.registrationDate = registrationDate;
+    }
+
+    public Date getExpirationDate()
+    {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate)
+    {
+        this.expirationDate = expirationDate;
+    }
+}
