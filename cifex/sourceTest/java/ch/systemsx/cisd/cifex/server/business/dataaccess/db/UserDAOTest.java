e8e2e18d8131545665b60e7a01560e7f907f2fe6
bneff <bneff>
2008-01-25 08:08:44 +0000

    Test added to recognize the Bug for tryFindUserByEmail, TestCases splittet in several methods
    SVN: 3783

--- cifex/sourceTest/java/ch/systemsx/cisd/cifex/server/business/dataaccess/db/UserDAOTest.java
+++ cifex/sourceTest/java/ch/systemsx/cisd/cifex/server/business/dataaccess/db/UserDAOTest.java
@@ -16,6 +16,9 @@
 
 package ch.systemsx.cisd.cifex.server.business.dataaccess.db;
 
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.fail;
+
 import java.util.Date;
 import java.util.List;
 
@@ -35,6 +38,10 @@ import ch.systemsx.cisd.cifex.server.business.dto.UserDTO;
     { "db", "user" })
 public final class UserDAOTest extends AbstractDAOTest
 {
+
+    final UserDTO testTemporaryUser = createUser(false, false, null);
+
+    final UserDTO testAdminUser = createUser(true, true, "admin@systemsx.ch");
 
     final static String getTestUserName()
     {
@@ -58,7 +65,7 @@ public final class UserDAOTest extends AbstractDAOTest
         user.setAdmin(admin);
         user.setPermanent(permanent);
         user.setRegistrationDate(new Date(new Long("402218403000").longValue()));
-        if (permanent)
+        if (permanent == false)
         {
             user.setExpirationDate(new Date(new Long("1222249782000").longValue()));
         }
@@ -67,12 +74,10 @@ public final class UserDAOTest extends AbstractDAOTest
 
     @Test
     @Transactional
-    public final void testUserDAO()
+    public final void testCreateUser()
     {
         IUserDAO userDAO = daoFactory.getUserDAO();
         List<UserDTO> listUsers = userDAO.listUsers();
-        UserDTO testAdminUser = createUser(true, true, "admin@systemsx.ch");
-        UserDTO testTemporaryUser = createUser(false, false, null);
         try
         {
             // Try with <code>null</code>
@@ -92,7 +97,15 @@ public final class UserDAOTest extends AbstractDAOTest
         userDAO.createUser(testTemporaryUser);
         assertEquals(listUsers.size() + 2, userDAO.listUsers().size());
 
-        // Get a user given its email.
+        setComplete();
+    }
+
+    @Test(dependsOnMethods =
+        { "testCreateUser" })
+    @Transactional
+    public final void testTryFindUserByEmail()
+    {
+        IUserDAO userDAO = daoFactory.getUserDAO();
         try
         {
             assert userDAO.tryFindUserByEmail(null) != null;
@@ -102,6 +115,11 @@ public final class UserDAOTest extends AbstractDAOTest
             assertEquals("No email specified!", e.getMessage());
         }
 
+        // Unknown Mail Address
+        UserDTO testUnknownUserFromDB = userDAO.tryFindUserByEmail("unknown@cisd.user");
+        assertEquals("Unknown user is not null", null, testUnknownUserFromDB);
+
+        // Existing admin User
         UserDTO testAdminUserFromDB = userDAO.tryFindUserByEmail(testAdminUser.getEmail());
         assert testAdminUserFromDB != null;
         assert testAdminUserFromDB.getID() != null;
@@ -109,14 +127,31 @@ public final class UserDAOTest extends AbstractDAOTest
 
         assertEquals(testAdminUser, testAdminUserFromDB);
 
-        // Delete user
+        // Existing Temporary User
+        UserDTO testTemporaryUserFromDB = userDAO.tryFindUserByEmail(testTemporaryUser.getEmail());
+        assert testTemporaryUserFromDB != null;
+        assert testTemporaryUserFromDB.getID() != null;
+        assert testTemporaryUserFromDB.getID() > 0;
+
+        assertEquals(testTemporaryUser, testTemporaryUserFromDB);
+    }
+
+    @Test(dependsOnMethods =
+        { "testTryFindUserByEmail" })
+    @Transactional
+    public final void testDeleteUser()
+    {
+        IUserDAO userDAO = daoFactory.getUserDAO();
+
+        List<UserDTO> listUsers = userDAO.listUsers();
+
+        assertFalse(userDAO.removeUser(new Long(-1)));
+
         assertTrue(userDAO.removeUser(testAdminUser.getID()));
-        assertEquals(listUsers.size() + 1, userDAO.listUsers().size());
+        assertEquals(listUsers.size() - 1, userDAO.listUsers().size());
 
         assertTrue(userDAO.removeUser(testTemporaryUser.getID()));
-        assertEquals(listUsers.size(), userDAO.listUsers().size());
-
-        setComplete();
+        assertEquals(listUsers.size() - 2, userDAO.listUsers().size());
     }
 
 }
