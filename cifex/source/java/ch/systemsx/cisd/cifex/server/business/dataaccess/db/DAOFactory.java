e212506797125315c7efedd6fb4bcad565b9784f
brinn <brinn>
2008-07-01 09:35:14 +0000

    [CFX-113] change: migrate to database version 005 which has a length of 120 rather than 50 for content types (allow to save the new Microsoft OpenXML formats like application/vnd.openxmlformats-officedocument.presentationml.presentation)
    SVN: 6938

--- cifex/source/java/ch/systemsx/cisd/cifex/server/business/dataaccess/db/DAOFactory.java
+++ cifex/source/java/ch/systemsx/cisd/cifex/server/business/dataaccess/db/DAOFactory.java
@@ -33,7 +33,7 @@ import ch.systemsx.cisd.dbmigration.DatabaseConfigurationContext;
 public class DAOFactory implements IDAOFactory
 {
     /** Current version of the database. */
-    public static final String DATABASE_VERSION = "004";
+    public static final String DATABASE_VERSION = "005";
 
     private final IUserDAO userDAO;
 
---
+++ cifex/source/sql/h2/migration/migration-004-005.sql
@@ -0,0 +1,9 @@
+----------------------------------------------------------------------
+--
+--  Migration script from version 4 to 5 of the database
+--
+----------------------------------------------------------------------
+
+CREATE DOMAIN CONTENT_TYPE as VARCHAR(120);
+ALTER TABLE FILES ALTER COLUMN CONTENT_TYPE CONTENT_TYPE;
+DROP DOMAIN VARCHAR_50;
