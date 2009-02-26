2e8b94d784ecedbfd0f2d13d709bc881e532fac0
felmer <felmer>
2009-02-26 09:32:09 +0000

    CFX-123 bug in DOS script fixed
    SVN: 10007

--- cifex/dist/client/cifex.bat
+++ cifex/dist/client/cifex.bat
@@ -1,4 +1,4 @@
 @echo off
 
-cifex_root=%~d0%~p0
-java -Xmx256m -Djavax.net.ssl.trustStore=etc\keystore -Dcifex.root=%cifex_root% -jar lib\cifex.jar %*
+
+java -Xmx256m -Djavax.net.ssl.trustStore=%~d0%~p0\etc\keystore -Dcifex.root=%~d0%~p0 -jar %~d0%~p0\lib\cifex.jar %*
