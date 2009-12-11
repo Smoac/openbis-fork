ce0b4c155e159205d6a1d8dd148920d1bddcf8a0
tpylak <tpylak>
2009-12-11 15:12:35 +0000

    CFX-165 Allow to show a message on the login page without server restart
    SVN: 13892

--- cifex/source/java/ch/systemsx/cisd/cifex/client/application/ui/LoginWidget.java
+++ cifex/source/java/ch/systemsx/cisd/cifex/client/application/ui/LoginWidget.java
@@ -20,193 +20,219 @@ import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.widget.MessageBox;
+import com.extjs.gxt.ui.client.widget.Text;
 import com.extjs.gxt.ui.client.widget.VerticalPanel;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.form.Field;
 import com.extjs.gxt.ui.client.widget.form.FormPanel;
 import com.extjs.gxt.ui.client.widget.form.TextField;
+import com.extjs.gxt.ui.client.widget.layout.TableData;
 
 import ch.systemsx.cisd.cifex.client.application.AbstractAsyncCallback;
 import ch.systemsx.cisd.cifex.client.application.IMessageResources;
 import ch.systemsx.cisd.cifex.client.application.Model;
 import ch.systemsx.cisd.cifex.client.application.ViewContext;
+import ch.systemsx.cisd.cifex.client.application.utils.CifexDict;
 import ch.systemsx.cisd.cifex.shared.basic.dto.UserInfoDTO;
 
 /**
- * A <code>Form</code> extension that is the login panel we use to login to the
- * <i>openBIS</i> system.
+ * A <code>Form</code> extension that is the login panel we use to login to the <i>openBIS</i>
+ * system.
  * 
  * @author Christian Ribeaud
  */
-public class LoginWidget extends VerticalPanel {
+public class LoginWidget extends VerticalPanel
+{
 
-	private final ViewContext context;
+    private final ViewContext context;
 
-	private final TextField<String> userField;
+    private final TextField<String> userField;
 
-	private final TextField<String> passwordField;
+    private final TextField<String> passwordField;
 
-	private final Button button;
+    private final Button button;
 
-	private final FormPanel formPanel;
+    private final FormPanel formPanel;
 
-	public LoginWidget(final ViewContext context) {
-		this.context = context;
-		// setSpacing(10);
+    public LoginWidget(final ViewContext context)
+    {
+        this.context = context;
+        // setSpacing(10);
 
-		formPanel = createFormPanel();
-		userField = createUserField();
-		formPanel.add(userField);
-		passwordField = createPasswordField();
-		formPanel.add(passwordField);
-		button = createButton();
-		formPanel.addButton(button);
+        formPanel = createFormPanel();
+        userField = createUserField();
+        formPanel.add(userField);
+        passwordField = createPasswordField();
+        formPanel.add(passwordField);
+        button = createButton();
+        formPanel.addButton(button);
 
-		// NOTE: it would be better to invoke it on reset but it somehow doesn't
-		// have any effect
-		focusOnFirstField();
+        // NOTE: it would be better to invoke it on reset but it somehow doesn't
+        // have any effect
+        focusOnFirstField();
 
-		add(formPanel);
-	}
+        add(formPanel);
 
-	private final Button createButton() {
-		final Button b = new Button(context.getMessageResources()
-				.getLoginButtonLabel());
-		b.addSelectionListener(new SelectionListener<ButtonEvent>() {
-			@Override
-			public void componentSelected(ButtonEvent ce) {
-				doLogin(context);
-			}
-		});
-		return b;
-	}
+        Text label = new Text(CifexDict.get(CifexDict.WELCOME_NOTE));
+        label.setStyleName("cifex-welcome-warning");
 
-	private void focusOnFirstField() {
-		formPanel.getItem(0).focus();
-	}
+        add(label);
+    }
 
-	private final TextField<String> createUserField() {
-		final TextField<String> field = new TextField<String>();
-		field.setFieldLabel(context.getMessageResources().getLoginUserLabel());
-		field.setAllowBlank(false);
-		field.setValidateOnBlur(true);
-		addEnterKeyListener(field, context);
-		return field;
-	}
+    private final Button createButton()
+    {
+        final Button b = new Button(context.getMessageResources().getLoginButtonLabel());
+        b.addSelectionListener(new SelectionListener<ButtonEvent>()
+            {
+                @Override
+                public void componentSelected(ButtonEvent ce)
+                {
+                    doLogin(context);
+                }
+            });
+        return b;
+    }
 
-	private final void addEnterKeyListener(final Field<String> field,
-			final ViewContext viewContext) {
-		field.addKeyListener(new EnterKeyListener() {
+    private void focusOnFirstField()
+    {
+        formPanel.getItem(0).focus();
+    }
 
-			@Override
-			protected final void onEnterKey() {
-				doLogin(viewContext);
-			}
-		});
-	}
+    private final TextField<String> createUserField()
+    {
+        final TextField<String> field = new TextField<String>();
+        field.setFieldLabel(context.getMessageResources().getLoginUserLabel());
+        field.setAllowBlank(false);
+        field.setValidateOnBlur(true);
+        addEnterKeyListener(field, context);
+        return field;
+    }
 
-	public final void resetFields() {
-		userField.reset();
-		passwordField.reset();
-		button.enable();
-	}
+    private final void addEnterKeyListener(final Field<String> field, final ViewContext viewContext)
+    {
+        field.addKeyListener(new EnterKeyListener()
+            {
 
-	private final TextField<String> createPasswordField() {
-		final TextField<String> field = new TextField<String>();
-		field.setPassword(true);
-		field.setFieldLabel(context.getMessageResources()
-				.getLoginPasswordLabel());
-		field.setAllowBlank(false);
-		field.setValidateOnBlur(true);
-		addEnterKeyListener(field, context);
-		return field;
-	}
+                @Override
+                protected final void onEnterKey()
+                {
+                    doLogin(viewContext);
+                }
+            });
+    }
 
-	/** Returns the button that will starts the login process. */
-	public final Button getButton() {
-		return button;
-	}
+    public final void resetFields()
+    {
+        userField.reset();
+        passwordField.reset();
+        button.enable();
+    }
 
-	@Override
-	protected final void onLoad() {
-		super.onLoad();
-		resetFields();
-	}
+    private final TextField<String> createPasswordField()
+    {
+        final TextField<String> field = new TextField<String>();
+        field.setPassword(true);
+        field.setFieldLabel(context.getMessageResources().getLoginPasswordLabel());
+        field.setAllowBlank(false);
+        field.setValidateOnBlur(true);
+        addEnterKeyListener(field, context);
+        return field;
+    }
 
-	private final void doLogin(final ViewContext viewContext) {
-		if (formPanel.isValid()) {
-			button.disable();
-			final String user = userField.getValue();
-			final String password = passwordField.getValue();
-			viewContext.getCifexService().tryLogin(user, password,
-					new LoginAsyncCallBack());
-		}
-	}
+    /** Returns the button that will starts the login process. */
+    public final Button getButton()
+    {
+        return button;
+    }
 
-	/**
-	 * Method that gets called when everything goes right.
-	 * <p>
-	 * By default this method does nothing.
-	 * </p>
-	 */
-	protected void loginSuccessful(final UserInfoDTO user) {
-		final Model model = context.getModel();
-		model.setUser(user);
-		if (FileDownloadHelper.startFileDownload(model))
-			context.getPageController().createInboxPage();
-		else
-			context.getPageController().createSharePage();
-	}
+    @Override
+    protected final void onLoad()
+    {
+        super.onLoad();
+        resetFields();
+    }
 
-	public final TextField<String> getUserField() {
-		return userField;
-	}
+    private final void doLogin(final ViewContext viewContext)
+    {
+        if (formPanel.isValid())
+        {
+            button.disable();
+            final String user = userField.getValue();
+            final String password = passwordField.getValue();
+            viewContext.getCifexService().tryLogin(user, password, new LoginAsyncCallBack());
+        }
+    }
 
-	public final TextField<String> getPasswordField() {
-		return passwordField;
-	}
+    /**
+     * Method that gets called when everything goes right.
+     * <p>
+     * By default this method does nothing.
+     * </p>
+     */
+    protected void loginSuccessful(final UserInfoDTO user)
+    {
+        final Model model = context.getModel();
+        model.setUser(user);
+        if (FileDownloadHelper.startFileDownload(model))
+            context.getPageController().createInboxPage();
+        else
+            context.getPageController().createSharePage();
+    }
 
-	private final static FormPanel createFormPanel() {
-		final FormPanel formPanel = new FormPanel();
-		// formPanel.setStyleName("cifex-login-widget");
-		formPanel.setBodyBorder(false);
-		formPanel.setBorders(false);
-		formPanel.setHeaderVisible(false);
-		formPanel.setFieldWidth(130);
-		formPanel.setWidth(250);
-		formPanel.setButtonAlign(HorizontalAlignment.RIGHT);
-		return formPanel;
-	}
+    public final TextField<String> getUserField()
+    {
+        return userField;
+    }
 
-	//
-	// Helper classes
-	//
+    public final TextField<String> getPasswordField()
+    {
+        return passwordField;
+    }
 
-	private final class LoginAsyncCallBack extends
-			AbstractAsyncCallback<UserInfoDTO> {
+    private final static FormPanel createFormPanel()
+    {
+        final FormPanel formPanel = new FormPanel();
+        // formPanel.setStyleName("cifex-login-widget");
+        formPanel.setBodyBorder(false);
+        formPanel.setBorders(false);
+        formPanel.setHeaderVisible(false);
+        formPanel.setFieldWidth(130);
+        formPanel.setWidth(250);
+        formPanel.setButtonAlign(HorizontalAlignment.RIGHT);
+        return formPanel;
+    }
 
-		LoginAsyncCallBack() {
-			super(context);
-		}
+    //
+    // Helper classes
+    //
 
-		@Override
-		public final void onFailure(final Throwable caught) {
-			super.onFailure(caught);
-			getButton().enable();
-		}
+    private final class LoginAsyncCallBack extends AbstractAsyncCallback<UserInfoDTO>
+    {
 
-		public final void onSuccess(final UserInfoDTO result) {
-			if (result != null) {
-				loginSuccessful(result);
-			} else {
-				final IMessageResources messageResources = context
-						.getMessageResources();
-				final String title = messageResources
-						.getMessageBoxWarningTitle();
-				MessageBox.alert(title, messageResources
-						.getLoginFailedMessage(), null);
-				getButton().enable();
-			}
-		}
-	}
+        LoginAsyncCallBack()
+        {
+            super(context);
+        }
+
+        @Override
+        public final void onFailure(final Throwable caught)
+        {
+            super.onFailure(caught);
+            getButton().enable();
+        }
+
+        public final void onSuccess(final UserInfoDTO result)
+        {
+            if (result != null)
+            {
+                loginSuccessful(result);
+            } else
+            {
+                final IMessageResources messageResources = context.getMessageResources();
+                final String title = messageResources.getMessageBoxWarningTitle();
+                MessageBox.alert(title, messageResources.getLoginFailedMessage(), null);
+                getButton().enable();
+            }
+        }
+    }
 }
