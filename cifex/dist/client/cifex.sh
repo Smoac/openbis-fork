46255d9c564303d0a4f98d4400bdad75d137efa8
brinn <brinn>
2010-01-30 17:40:19 +0000

    change: make cifex.sh follow symlinks
    SVN: 14576

--- cifex/dist/client/cifex.sh
+++ cifex/dist/client/cifex.sh
@@ -1,5 +1,18 @@
 #! /bin/sh
 
-CIFEX_ROOT=`dirname $0`
+# This script requires the readlink binary. If your system lacks this binary, $JHDFDIR needs to be hard-coded
+
+SCRIPT="$0"
+BINDIR="${SCRIPT%/*}"
+LINK="`readlink $0`"
+while [ -n "${LINK}" ]; do
+  if [ "${LINK#/}" = "${LINK}" ]; then
+    SCRIPT="${BINDIR}/${LINK}"
+  else
+    SCRIPT="${LINK}"
+  fi
+  LINK="`readlink ${SCRIPT}`"
+done
+CIFEX_ROOT="`dirname ${SCRIPT}`"
 
 java -Xmx256m -Djavax.net.ssl.trustStore=${CIFEX_ROOT}/etc/keystore -Dcifex.root=${CIFEX_ROOT} -jar ${CIFEX_ROOT}/lib/cifex.jar "$@"
