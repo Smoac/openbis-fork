a8b66e03f0f64e68e663c08a4b96bb31ea8dd630
brinn <brinn>
2010-01-30 17:14:44 +0000

    change: simplify
    SVN: 14573

--- cifex/dist/client/cifex.bat
+++ cifex/dist/client/cifex.bat
@@ -1,4 +1,3 @@
 @echo off
 
-
-java -Xmx256m -Djavax.net.ssl.trustStore=%~d0%~p0\etc\keystore -Dcifex.root=%~d0%~p0 -jar %~d0%~p0\lib\cifex.jar %*
+java -Xmx256m -Djavax.net.ssl.trustStore=%~dp0etc\keystore -Dcifex.root=%~dp0 -jar %~dp0lib\cifex.jar %*
