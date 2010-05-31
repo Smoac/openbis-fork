4f8566abcfd6f8fb224887f9dd4d62ae29fc0da4
brinn <brinn>
2010-05-31 07:16:13 +0000

    add: comment on how to best format warnings
    SVN: 16203

--- cifex/source/java/ch/systemsx/cisd/cifex/public/info-dictionary.js
+++ cifex/source/java/ch/systemsx/cisd/cifex/public/info-dictionary.js
@@ -1,10 +1,11 @@
 // All message keys should be in upper case.
 var info_dict = {
 
-  // Shown below login form. Use HTML tags for formatting the start-page message
+  // Shown below login form. Use HTML tags for formatting the start-page message, e.g.
+  // <span style="color:red; font-size:large; font-weight:bold;">A warning!</span>
   START_PAGE_WELCOME_NOTE: '', 
  
-  // Email address of the service administrator / helpdesk.
+  // Email address of the service administrator / support / helpdesk.
   SUPPORT_EMAIL: 'root@localhost',
   
   // Hyperlink to web page describing the software / service.
