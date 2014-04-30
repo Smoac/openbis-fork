3fc558f0de8d6d85fdf4a15790677ce402f4fb6b
jakubs <jakubs>
2014-04-30 12:37:44 +0000

    fix the problem with not working antrun
    SVN: 31439

--- hcscld/build/antrun.sh
+++ hcscld/build/antrun.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 
-SCRIPT="$0"
-if [ "${SCRIPT}" = "${SCRIPT#/}" ]; then
-  SCRIPT="`pwd`/${SCRIPT#./}"
-fi
-BINDIR="${SCRIPT%/*}"
-BASEDIR=${BINDIR%/*}
-cd $BASEDIR
-ant -lib ../../build_resources/lib/ecj.jar -f build/build.xml "$@"
+ME="$0"
+MYDIR=${ME%/*}
+cd $MYDIR
+ant -lib ../../build_resources/lib/ecj.jar "$@"
