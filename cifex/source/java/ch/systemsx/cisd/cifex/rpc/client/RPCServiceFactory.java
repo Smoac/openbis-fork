22a5a0aa670601bbca4d7344adbaa56822689791
anttil <anttil>
2015-02-17 14:52:40 +0000

    SSDM-1507: Fix CIFEX connectivity
    SVN: 33472

--- cifex/source/java/ch/systemsx/cisd/cifex/rpc/client/RPCServiceFactory.java
+++ cifex/source/java/ch/systemsx/cisd/cifex/rpc/client/RPCServiceFactory.java
@@ -20,22 +20,9 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.net.InetSocketAddress;
-
-import org.apache.commons.lang.time.DateUtils;
-import org.apache.http.HttpHost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.remoting.httpinvoker.HttpComponentsHttpInvokerRequestExecutor;
-
-import com.marathon.util.spring.StreamSupportingHttpInvokerProxyFactoryBean;
 
 import ch.systemsx.cisd.cifex.rpc.ICIFEXRPCService;
-import ch.systemsx.cisd.common.logging.LogInitializer;
 import ch.systemsx.cisd.common.spring.HttpInvokerUtils;
-import ch.systemsx.cisd.common.ssl.SslCertificateHelper;
 
 /**
  * The factory for the CIFEX RPC service.
@@ -44,8 +31,6 @@ import ch.systemsx.cisd.common.ssl.SslCertificateHelper;
  */
 public final class RPCServiceFactory
 {
-
-    private static final String SPRING_BEAN_URL_PROTOCOL = "spring-bean://";
 
     private static final int SERVER_TIMEOUT_MIN = 5;
 
@@ -87,7 +72,7 @@ public final class RPCServiceFactory
         final ServiceInvocationHandler invocationHandler = new ServiceInvocationHandler(service);
         final ICIFEXRPCService proxy =
                 (ICIFEXRPCService) Proxy.newProxyInstance(classLoader, new Class[]
-                    { ICIFEXRPCService.class }, invocationHandler);
+                { ICIFEXRPCService.class }, invocationHandler);
         final int apiServerVersion = proxy.getVersion();
         final int apiMinClientVersion = proxy.getMinClientVersion();
         if (ICIFEXRPCService.VERSION < apiMinClientVersion
@@ -99,49 +84,9 @@ public final class RPCServiceFactory
         return new CIFEXComponent(proxy);
     }
 
-    // TODO 2010-04-21, Tomasz Pylak: refactor to use
-    // HttpInvokerUtils.createStreamSupportingServiceStub()
     private static ICIFEXRPCService createService(String serviceURL,
             boolean getServerCertificateFromServer)
     {
-        if (serviceURL.startsWith(SPRING_BEAN_URL_PROTOCOL))
-        {
-            AbstractApplicationContext applicationContext =
-                    new ClassPathXmlApplicationContext(new String[]
-                        { "applicationContext.xml" }, true);
-            LogInitializer.init();
-            return ((ICIFEXRPCService) applicationContext.getBean("rpc-service"));
-        }
-        if (getServerCertificateFromServer)
-        {
-            setUpKeyStore(serviceURL);
-        }
-        final StreamSupportingHttpInvokerProxyFactoryBean httpInvokerProxy =
-                new StreamSupportingHttpInvokerProxyFactoryBean();
-        httpInvokerProxy.setServiceUrl(serviceURL);
-        httpInvokerProxy.setServiceInterface(ICIFEXRPCService.class);
-        
-        HttpComponentsHttpInvokerRequestExecutor httpInvokerRequestExecutor;        
-        final InetSocketAddress proxyAddressOrNull = HttpInvokerUtils.tryFindProxy(serviceURL);
-        if (proxyAddressOrNull != null)
-        {
-            HttpHost proxy = new HttpHost(proxyAddressOrNull.getHostName(), proxyAddressOrNull.getPort(), "http");
-            CloseableHttpClient client = HttpClients.custom().setProxy(proxy).build();
-            httpInvokerRequestExecutor = new HttpComponentsHttpInvokerRequestExecutor(client);            
-        } else {
-            httpInvokerRequestExecutor = new HttpComponentsHttpInvokerRequestExecutor();                        
-        }
-        
-        httpInvokerRequestExecutor.setReadTimeout((int) DateUtils.MILLIS_PER_MINUTE * SERVER_TIMEOUT_MIN);
-        httpInvokerProxy.setHttpInvokerRequestExecutor(httpInvokerRequestExecutor);
-        httpInvokerProxy.afterPropertiesSet();
-        return (ICIFEXRPCService) httpInvokerProxy.getObject();
+        return HttpInvokerUtils.createStreamSupportingServiceStub(ICIFEXRPCService.class, serviceURL, SERVER_TIMEOUT_MIN * 1000L);
     }
-
-    private static void setUpKeyStore(String serviceURL)
-    {
-        new SslCertificateHelper(serviceURL, ClientConfigurationFiles.KEYSTORE_FILE, "cifex")
-                .setUpKeyStore();
-    }
-
 }
