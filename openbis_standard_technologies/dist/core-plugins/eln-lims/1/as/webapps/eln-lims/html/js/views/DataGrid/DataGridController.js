9d1c27a3a52edc5c981116ba8ca64f4b20ad688e
anttil <anttil>
2016-06-10 09:36:58 +0000

    SSDM-3664: S232 openBIS Merge S231 to stage SSDM-3720: Regression Bug on Search - Lucene index parser broken (36621, 36625, 36627) SSDM-3617: Maintenance task to find orphan datasets in archives (36628, 36630, 36631, 36632, 36633, 36634, 36637, 36638, 36640, 36641, 36648, 36650) SSDM-3703: max size of project in standard UI in Edit mode still set to 2000 characters (36629) SSDM-3622: ELN: improve search (36598) SSDM-3632: ELN: multi-select parents and multi-select samples for deletion (36639, 36642, 36644, 36653, 36655, 36657) SSDM-3086: ELN : Improvements in tables (36494) SSDM-3623: ELN Support for Ciaudo (36536) SSDM-3655: S231 Project Presentation: ELN (36663, 36664) r36626: login form update r36643: bacteria_antibiotic_resistance vocabulary corrected
    SVN: 36667

--- openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/views/DataGrid/DataGridController.js
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/views/DataGrid/DataGridController.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-function DataGridController(title, columns, data, rowClickEventHandler, showAllColumns,configKey) {
+function DataGridController(title, columns, data, rowClickEventHandler, showAllColumns,configKey, isMultiselectable) {
 	this._grid = null;
 	this._dataGridModel = null;
 	this._dataGridView = null;
@@ -24,15 +24,15 @@ function DataGridController(title, columns, data, rowClickEventHandler, showAllC
 		window.alert("[TO-DELETE] Empty configKey during the table init, this should never happen, tell the developers.");
 	}
 	
-	this.init = function($container) {
+	this.init = function($container, extraOptions) {
 		var webAppId = "ELN-LIMS";
 		mainController.serverFacade.openbisServer.getWebAppSettings(webAppId, function(response) {
 			var settings = response.result.settings;
-			var onColumnsChange = function(columnsModel) {
+			var onColumnsChange = function(tableState) {
 				if(!settings) {
 					settings = {};
 				}
-				settings[configKey] = JSON.stringify(columnsModel);
+				settings[configKey] = JSON.stringify(tableState);
 				
 				var webAppSettings = {
 						"@type" : "WebAppSettings",
@@ -45,14 +45,20 @@ function DataGridController(title, columns, data, rowClickEventHandler, showAllC
 			var tableSettings = null;
 			if(configKey && settings[configKey]) {
 				tableSettings = JSON.parse(settings[configKey]);
+				if(Object.keys(tableSettings).length > 3) { //Clean old settings
+					tableSettings = null;
+				}
 			}
-			_this._grid = new Grid(columns, data, showAllColumns, tableSettings, onColumnsChange);
+			_this._grid = new Grid(columns, data, showAllColumns, tableSettings, onColumnsChange, isMultiselectable);
 			if(rowClickEventHandler) {
 				_this._grid.addRowClickListener(rowClickEventHandler);
 			}
-			_this._dataGridModel = new DataGridModel(title, columns, data, rowClickEventHandler, _this._grid.render());
-			_this._dataGridView = new DataGridView(this, _this._dataGridModel);
+			if(extraOptions) {
+				_this._grid.addExtraOptions(extraOptions);
+			}
 			
+			_this._dataGridModel = new DataGridModel(title, columns, data, rowClickEventHandler, _this._grid.render(), isMultiselectable);
+			_this._dataGridView = new DataGridView(this, _this._dataGridModel);
 			_this._dataGridView.repaint($container);
 		});	
 	}
