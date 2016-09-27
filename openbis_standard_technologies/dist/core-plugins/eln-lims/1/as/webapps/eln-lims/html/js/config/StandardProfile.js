a100d9473926234e6e768951aa25d379a334357b
juanf <juanf>
2016-09-27 08:52:32 +0000

    S239 bugfixes to stage found during testing SSDM-4150 : ELN Ordering fixes from feedback and testing
    SVN: 37121

--- openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/config/StandardProfile.js
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/config/StandardProfile.js
@@ -454,7 +454,7 @@ $.extend(StandardProfile.prototype, DefaultProfile.prototype, {
 							var requestProductAnnotations = requestProductsAnnotations[requestProduct.permId];
 							
 							if(requestProduct.parents.length === 0) {
-								Util.showError("Product " + requestProduct.code + " don't have a provider, FIX IT!.");
+								Util.showError("Product " + requestProduct.code + " does not have a provider, FIX IT!.");
 								return;
 							}
 							var provider = requestProduct.parents[0];
@@ -470,7 +470,7 @@ $.extend(StandardProfile.prototype, DefaultProfile.prototype, {
 							}
 							quantity += parseInt(requestProductAnnotations["QUANTITY_OF_ITEMS"]);
 							if(!quantity) {
-								Util.showError("Product " + requestProduct.code + " from request " +  request.code + " don't have a quantity, FIX IT!.");
+								Util.showError("Product " + requestProduct.code + " from request " +  request.code + " does not have a quantity, FIX IT!.");
 								return;
 							}
 							
@@ -513,7 +513,7 @@ $.extend(StandardProfile.prototype, DefaultProfile.prototype, {
 							
 							var page = languageLabels["DATE_LABEL"] + ": " + Util.getFormatedDate(new Date(registrationDate));
 								page += "\n";
-								page += languageLabels["SUPPLIER_LABEL"] + ": " + provider.properties["NAME"];
+								page += languageLabels["SUPPLIER_LABEL"] + ": " + provider.properties["COMPANY_NAME"];
 								page += "\n";
 								page += languageLabels["CONTACT_INFO_LABEL"] + ":";
 								page += "\n";
@@ -631,7 +631,7 @@ $.extend(StandardProfile.prototype, DefaultProfile.prototype, {
 								
 								var rowData = {};
 								rowData.permId = product.permId;
-								rowData.supplier = provider.properties["NAME"];
+								rowData.supplier = provider.properties["COMPANY_NAME"];
 								rowData.name = product.properties["NAME"];
 								rowData.code =  product.properties["CATALOG_CODE"];
 								rowData.quantity = quantity;
