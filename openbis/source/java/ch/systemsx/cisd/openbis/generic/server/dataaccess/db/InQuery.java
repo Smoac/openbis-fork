8b2a3dba72c865d1843be00bd48d22fa21a682a6
anttil <anttil>
2016-10-27 14:35:32 +0000

    SSDM-4259: S242 openBIS Merge S241 to stage SSDM-4192: SELECT IN bug fixed (part of: "NEXUS cellario-relations crashing") (37145, 37147, 37148, 37149, 37150) SSDM-4149: ELN: escape white space characters (37152) SSDM-4209: ELN ORDERING: print separate order files per each company (37178) SSDM-4218: ELN: issue with search field	Bug (37170) SSDM-4224: ELN: modify behaviour of experimental steps shown in main menu (37179) SSDM-4232: Fix MULTILINE_VARCHAR Rich Text to VARCHAR plain text (37182) SSDM-4276: ELN installation with minimal masterdata does not work (37183) SSDM-4277: Yeastlab Support (37177)
    SVN: 37211

---
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/InQuery.java
@@ -0,0 +1,47 @@
+package ch.systemsx.cisd.openbis.generic.server.dataaccess.db;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+
+public class InQuery<I, O>
+{
+    private static final int POSTGRES_DRIVER_MAX_ARGS = 32767; // Uses a signed 2 bytes integer
+
+    public List<O> withBatch(Session session, String inQuery, String inParameter, List<I> inArguments, Map<String, Object> fixParams)
+    {
+        List<O> result = new ArrayList<O>(inArguments.size());
+
+        int fixParamsSize = (fixParams == null) ? 0 : fixParams.size();
+        int fromIndex = 0;
+
+        while (fromIndex < inArguments.size())
+        {
+            int toIndex = fromIndex + POSTGRES_DRIVER_MAX_ARGS - fixParamsSize;
+            if (toIndex > inArguments.size())
+            {
+                toIndex = inArguments.size();
+            }
+
+            List<I> partialInArguments = inArguments.subList(fromIndex, toIndex);
+            SQLQuery query = session.createSQLQuery(inQuery);
+            query.setParameterList(inParameter, partialInArguments);
+
+            if (fixParams != null)
+            {
+                for (String paramName : fixParams.keySet())
+                {
+                    query.setParameter(paramName, fixParams.get(paramName));
+                }
+            }
+            List<O> partialResult = query.list();
+            result.addAll(partialResult);
+            fromIndex = toIndex;
+        }
+
+        return result;
+    }
+}
