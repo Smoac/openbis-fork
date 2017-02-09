5746efe0bc057b11c12812ee2d469508720b991a
juanf <juanf>
2017-02-09 15:33:03 +0000

    SSDM-4686 : v3 roles bug - DataSet predicates using collections for performance
    SVN: 37726

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/server/authorization/predicate/V3DataSetDeletePredicate.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/authorization/predicate/V3DataSetDeletePredicate.java
@@ -1,28 +1,31 @@
 package ch.systemsx.cisd.openbis.generic.server.authorization.predicate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ch.systemsx.cisd.common.exceptions.Status;
 import ch.systemsx.cisd.openbis.generic.server.authorization.IAuthorizationDataProvider;
 import ch.systemsx.cisd.openbis.generic.server.authorization.RoleWithIdentifier;
+import ch.systemsx.cisd.openbis.generic.server.authorization.annotation.ShouldFlattenCollections;
 import ch.systemsx.cisd.openbis.generic.server.authorization.predicate.v3ToV1.DataSetIdTranslator;
 import ch.systemsx.cisd.openbis.generic.shared.dto.PersonPE;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.dataset.id.IDataSetId;;
 
-public class V3DataSetDeletePredicate extends AbstractPredicate<IDataSetId>
+@ShouldFlattenCollections(value = false)
+public class V3DataSetDeletePredicate extends AbstractPredicate<List<IDataSetId>>
 {
 
-    protected final DataSetCodePredicate datasetCodePredicate;
+    protected final DataSetCodeCollectionPredicate datasetCodeCollectionPredicate;
 
     public V3DataSetDeletePredicate()
     {
-        this.datasetCodePredicate = new DataSetCodePredicate();
+        this.datasetCodeCollectionPredicate = new DataSetCodeCollectionPredicate();
     }
 
     @Override
     public final void init(IAuthorizationDataProvider provider)
     {
-        datasetCodePredicate.init(provider);
+    	datasetCodeCollectionPredicate.init(provider);
     }
 
     @Override
@@ -32,9 +35,13 @@ public class V3DataSetDeletePredicate extends AbstractPredicate<IDataSetId>
     }
 
     @Override
-    protected Status doEvaluation(PersonPE person, List<RoleWithIdentifier> allowedRoles, IDataSetId value)
+    protected Status doEvaluation(PersonPE person, List<RoleWithIdentifier> allowedRoles, List<IDataSetId> values)
     {
-        assert datasetCodePredicate.initialized : "Predicate has not been initialized";
-        return datasetCodePredicate.doEvaluation(person, allowedRoles, DataSetIdTranslator.translate(value));
+        assert datasetCodeCollectionPredicate.initialized : "Predicate has not been initialized";
+    	List<String> valuesAsCodes = new ArrayList<String>();
+    	for(IDataSetId value:values) {
+    		valuesAsCodes.add(DataSetIdTranslator.translate(value));
+    	}
+        return datasetCodeCollectionPredicate.doEvaluation(person, allowedRoles, valuesAsCodes);
     }
 }
