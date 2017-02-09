badc2dddc9c683fa83562ea3ea13205fcf2f4eb5
felmer <felmer>
2017-02-09 12:41:40 +0000

    SSDM-4686: using fully-qualified class name only when necessary
    SVN: 37721

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/server/authorization/predicate/v3ToV1/SampleIdTranslator.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/authorization/predicate/v3ToV1/SampleIdTranslator.java
@@ -1,5 +1,8 @@
 package ch.systemsx.cisd.openbis.generic.server.authorization.predicate.v3ToV1;
 
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.common.id.CreationId;
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.id.SampleIdentifier;
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.id.SamplePermId;
 import ch.systemsx.cisd.openbis.generic.shared.api.v1.dto.id.sample.ISampleId;
 import ch.systemsx.cisd.openbis.generic.shared.api.v1.dto.id.sample.SampleIdentifierId;
 import ch.systemsx.cisd.openbis.generic.shared.api.v1.dto.id.sample.SamplePermIdId;
@@ -9,15 +12,15 @@ public class SampleIdTranslator
 
     public static ISampleId translate(ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.id.ISampleId v3SampleId)
     {
-        if (v3SampleId instanceof ch.ethz.sis.openbis.generic.asapi.v3.dto.common.id.CreationId)
+        if (v3SampleId instanceof CreationId)
         {
 
-        } else if (v3SampleId instanceof ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.id.SampleIdentifier)
+        } else if (v3SampleId instanceof SampleIdentifier)
         {
-            return new SampleIdentifierId(((ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.id.SampleIdentifier) v3SampleId).getIdentifier());
-        } else if (v3SampleId instanceof ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.id.SamplePermId)
+            return new SampleIdentifierId(((SampleIdentifier) v3SampleId).getIdentifier());
+        } else if (v3SampleId instanceof SamplePermId)
         {
-            return new SamplePermIdId(((ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.id.SamplePermId) v3SampleId).getPermId());
+            return new SamplePermIdId(((SamplePermId) v3SampleId).getPermId());
         }
 
         return null;
