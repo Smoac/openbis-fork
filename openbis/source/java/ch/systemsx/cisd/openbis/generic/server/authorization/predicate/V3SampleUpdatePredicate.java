0acf72bedd0a73d422075324a556b28bbb83bfee
juanf <juanf>
2017-02-10 12:34:36 +0000

    SSDM-4686 : v3 roles bug - one more predicate fix, some reverted
    SVN: 37729

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/server/authorization/predicate/V3SampleUpdatePredicate.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/authorization/predicate/V3SampleUpdatePredicate.java
@@ -5,14 +5,11 @@ import java.util.List;
 import ch.systemsx.cisd.common.exceptions.Status;
 import ch.systemsx.cisd.openbis.generic.server.authorization.IAuthorizationDataProvider;
 import ch.systemsx.cisd.openbis.generic.server.authorization.RoleWithIdentifier;
-import ch.systemsx.cisd.openbis.generic.server.authorization.annotation.ShouldFlattenCollections;
 import ch.systemsx.cisd.openbis.generic.server.authorization.predicate.v3ToV1.SampleIdTranslator;
 import ch.systemsx.cisd.openbis.generic.shared.dto.PersonPE;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.update.SampleUpdate;
-import ch.systemsx.cisd.openbis.generic.shared.api.v1.dto.id.sample.ISampleId;
 
-@ShouldFlattenCollections(value = false)
-public class V3SampleUpdatePredicate extends AbstractPredicate<List<SampleUpdate>>
+public class V3SampleUpdatePredicate extends AbstractPredicate<SampleUpdate>
 {
 
     protected final SampleIdPredicate sampleIdPredicate;
@@ -35,17 +32,9 @@ public class V3SampleUpdatePredicate extends AbstractPredicate<List<SampleUpdate
     }
 
     @Override
-    protected Status doEvaluation(PersonPE person, List<RoleWithIdentifier> allowedRoles, List<SampleUpdate> value)
+    protected Status doEvaluation(PersonPE person, List<RoleWithIdentifier> allowedRoles, SampleUpdate value)
     {
         assert sampleIdPredicate.initialized : "Predicate has not been initialized";
-    	for(SampleUpdate sampleUpdate:value) {
-    		ISampleId sampleId = SampleIdTranslator.translate(sampleUpdate.getSampleId());
-    		Status result = sampleIdPredicate.doEvaluation(person, allowedRoles, sampleId);
-            if (result != Status.OK)
-            {
-                return result;
-            }
-    	}
-        return Status.OK;
+        return sampleIdPredicate.doEvaluation(person, allowedRoles, SampleIdTranslator.translate(value.getSampleId()));
     }
 }
