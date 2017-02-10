dadcc7ee9e7e628ffbda5184a7648e37a07d9a5c
juanf <juanf>
2017-02-10 10:39:56 +0000

    SSDM-4686 : v3 roles bug - Space predicates using collections for performance
    SVN: 37728

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/server/authorization/predicate/V3SpaceUpdatePredicate.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/authorization/predicate/V3SpaceUpdatePredicate.java
@@ -1,28 +1,35 @@
 package ch.systemsx.cisd.openbis.generic.server.authorization.predicate;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import ch.systemsx.cisd.common.exceptions.Status;
 import ch.systemsx.cisd.openbis.generic.server.authorization.IAuthorizationDataProvider;
 import ch.systemsx.cisd.openbis.generic.server.authorization.RoleWithIdentifier;
+import ch.systemsx.cisd.openbis.generic.server.authorization.annotation.ShouldFlattenCollections;
 import ch.systemsx.cisd.openbis.generic.server.authorization.predicate.v3ToV1.SpaceIdTranslator;
 import ch.systemsx.cisd.openbis.generic.shared.dto.PersonPE;
+import ch.systemsx.cisd.openbis.generic.shared.dto.identifier.SpaceIdentifier;
+import ch.systemsx.cisd.openbis.generic.shared.util.SpaceCodeHelper;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.space.update.SpaceUpdate;
 
-public class V3SpaceUpdatePredicate extends AbstractPredicate<SpaceUpdate>
+@ShouldFlattenCollections(value = false)
+public class V3SpaceUpdatePredicate extends AbstractPredicate<List<SpaceUpdate>>
 {
 
-    protected final SpaceIdentifierPredicate spacePredicate;
+    protected final SpaceCollectionPredicate spaceCollectionPredicate;
 
     public V3SpaceUpdatePredicate()
     {
-        this.spacePredicate = new SpaceIdentifierPredicate();
+        this.spaceCollectionPredicate = new SpaceCollectionPredicate();
     }
 
     @Override
     public final void init(IAuthorizationDataProvider provider)
     {
-        spacePredicate.init(provider);
+    	spaceCollectionPredicate.init(provider);
     }
 
     @Override
@@ -32,9 +39,15 @@ public class V3SpaceUpdatePredicate extends AbstractPredicate<SpaceUpdate>
     }
 
     @Override
-    protected Status doEvaluation(PersonPE person, List<RoleWithIdentifier> allowedRoles, SpaceUpdate value)
+    protected Status doEvaluation(PersonPE person, List<RoleWithIdentifier> allowedRoles, List<SpaceUpdate> value)
     {
-        assert spacePredicate.initialized : "Predicate has not been initialized";
-        return spacePredicate.doEvaluation(person, allowedRoles, SpaceIdTranslator.translate(value.getSpaceId()));
+        assert spaceCollectionPredicate.initialized : "Predicate has not been initialized";
+    	Set<String> spaceCodes = new HashSet<String>();
+    	for(SpaceUpdate spaceUpdate:value) {
+    		SpaceIdentifier spaceIdentifier = SpaceIdTranslator.translate(spaceUpdate.getSpaceId());
+    		final String spaceCode = SpaceCodeHelper.getSpaceCode(person, spaceIdentifier);
+    		spaceCodes.add(spaceCode);
+    	}
+        return spaceCollectionPredicate.doEvaluation(person, allowedRoles, new ArrayList<String>(spaceCodes));
     }
 }
