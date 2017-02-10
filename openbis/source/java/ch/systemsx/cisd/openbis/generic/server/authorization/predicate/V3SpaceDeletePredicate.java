1136f8bcb2c685de6a9f47fc3e56c5fff773864e
juanf <juanf>
2017-02-10 10:39:56 +0000

    SSDM-4686 : v3 roles bug - Space predicates using collections for performance
    SVN: 37728

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/server/authorization/predicate/V3SpaceDeletePredicate.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/authorization/predicate/V3SpaceDeletePredicate.java
@@ -1,28 +1,35 @@
 package ch.systemsx.cisd.openbis.generic.server.authorization.predicate;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import ch.systemsx.cisd.common.exceptions.Status;
 import ch.systemsx.cisd.openbis.generic.server.authorization.IAuthorizationDataProvider;
 import ch.systemsx.cisd.openbis.generic.server.authorization.RoleWithIdentifier;
+import ch.systemsx.cisd.openbis.generic.server.authorization.annotation.ShouldFlattenCollections;
 import ch.systemsx.cisd.openbis.generic.server.authorization.predicate.v3ToV1.SpaceIdTranslator;
 import ch.systemsx.cisd.openbis.generic.shared.dto.PersonPE;
+import ch.systemsx.cisd.openbis.generic.shared.dto.identifier.SpaceIdentifier;
+import ch.systemsx.cisd.openbis.generic.shared.util.SpaceCodeHelper;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.space.id.ISpaceId;
 
-public class V3SpaceDeletePredicate extends AbstractPredicate<ISpaceId>
+@ShouldFlattenCollections(value = false)
+public class V3SpaceDeletePredicate extends AbstractPredicate<List<ISpaceId>>
 {
 
-    protected final SpaceIdentifierPredicate spacePredicate;
+    protected final SpaceCollectionPredicate spaceCollectionPredicate;
 
     public V3SpaceDeletePredicate()
     {
-        this.spacePredicate = new SpaceIdentifierPredicate();
+        this.spaceCollectionPredicate = new SpaceCollectionPredicate();
     }
 
     @Override
     public final void init(IAuthorizationDataProvider provider)
     {
-        spacePredicate.init(provider);
+    	spaceCollectionPredicate.init(provider);
     }
 
     @Override
@@ -32,9 +39,15 @@ public class V3SpaceDeletePredicate extends AbstractPredicate<ISpaceId>
     }
 
     @Override
-    protected Status doEvaluation(PersonPE person, List<RoleWithIdentifier> allowedRoles, ISpaceId value)
+    protected Status doEvaluation(PersonPE person, List<RoleWithIdentifier> allowedRoles, List<ISpaceId> value)
     {
-        assert spacePredicate.initialized : "Predicate has not been initialized";
-        return spacePredicate.doEvaluation(person, allowedRoles, SpaceIdTranslator.translate(value));
+        assert spaceCollectionPredicate.initialized : "Predicate has not been initialized";
+    	Set<String> spaceCodes = new HashSet<String>();
+    	for(ISpaceId spaceId:value) {
+    		SpaceIdentifier spaceIdentifier = SpaceIdTranslator.translate(spaceId);
+    		final String spaceCode = SpaceCodeHelper.getSpaceCode(person, spaceIdentifier);
+    		spaceCodes.add(spaceCode);
+    	}
+        return spaceCollectionPredicate.doEvaluation(person, allowedRoles, new ArrayList<String>(spaceCodes));
     }
 }
