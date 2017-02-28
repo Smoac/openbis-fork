e7a2553f3b160dc3e2c954251a45716a4b7e51a8
felmer <felmer>
2017-02-28 14:13:59 +0000

    SSDM-4633: backporting SSDM-SSDM-4652 (createDataSetTypes) manually.
    SVN: 37833

---
+++ openbis/source/java/ch/ethz/sis/openbis/generic/server/asapi/v3/executor/method/ICreateDataSetTypeMethodExecutor.java
@@ -0,0 +1,9 @@
+package ch.ethz.sis.openbis.generic.server.asapi.v3.executor.method;
+
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.dataset.create.DataSetTypeCreation;
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.entitytype.id.EntityTypePermId;
+
+public interface ICreateDataSetTypeMethodExecutor extends ICreateMethodExecutor<EntityTypePermId, DataSetTypeCreation>
+{
+
+}
