681db59d07e384a6e4e63fcba731c29fb29aa0f0
felmer <felmer>
2017-11-14 13:40:03 +0000

    SSDM-5809: Manual backporting API DTO JS files for SSDM-4845.
    SVN: 38902

--- openbis/source/java/ch/systemsx/cisd/openbis/public/resources/api/v3/as/dto/entitytype/search/AbstractEntityTypeSearchCriteria.js
+++ openbis/source/java/ch/systemsx/cisd/openbis/public/resources/api/v3/as/dto/entitytype/search/AbstractEntityTypeSearchCriteria.js
@@ -1,17 +1,25 @@
 /**
  * @author pkupczyk
  */
-define([ "require", "stjs", "as/dto/common/search/AbstractObjectSearchCriteria", "as/dto/common/search/CodeSearchCriteria", "as/dto/common/search/PermIdSearchCriteria",
-		"as/dto/common/search/AbstractCompositeSearchCriteria" ], function(require, stjs, AbstractObjectSearchCriteria) {
+define([ "require", "stjs", "as/dto/common/search/AbstractObjectSearchCriteria", "as/dto/common/search/IdsSearchCriteria", "as/dto/common/search/CodeSearchCriteria", "as/dto/common/search/CodesSearchCriteria", 
+         "as/dto/common/search/PermIdSearchCriteria", "as/dto/common/search/AbstractCompositeSearchCriteria" ], function(require, stjs, AbstractObjectSearchCriteria) {
 	var AbstractEntityTypeSearchCriteria = function() {
 		AbstractObjectSearchCriteria.call(this);
 	};
 	stjs.extend(AbstractEntityTypeSearchCriteria, AbstractObjectSearchCriteria, [ AbstractObjectSearchCriteria ], function(constructor, prototype) {
 		prototype['@type'] = 'as.dto.entitytype.search.AbstractEntityTypeSearchCriteria';
 		constructor.serialVersionUID = 1;
+		prototype.withIds = function() {
+			var IdsSearchCriteria = require("as/dto/common/search/IdsSearchCriteria");
+			return this.addCriteria(new IdsSearchCriteria());
+		};
 		prototype.withCode = function() {
 			var CodeSearchCriteria = require("as/dto/common/search/CodeSearchCriteria");
 			return this.addCriteria(new CodeSearchCriteria());
+		};
+		prototype.withCodes = function() {
+			var CodesSearchCriteria = require("as/dto/common/search/CodesSearchCriteria");
+			return this.addCriteria(new CodesSearchCriteria());
 		};
 		prototype.withPermId = function() {
 			var PermIdSearchCriteria = require("as/dto/common/search/PermIdSearchCriteria");
