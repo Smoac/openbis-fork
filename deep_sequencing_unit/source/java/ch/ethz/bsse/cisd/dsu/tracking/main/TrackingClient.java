bc10b63634586e454663bb8d4ddd26027771923c
Juan Fuentes <juanf@bs-mbpr28.d.ethz.ch>
2019-11-11 15:38:31 +0100

    Revert "SSDM-8887 : Backport fixes to 18.06 + fix apache lang3 dependencies that are broken for some reason"
    This reverts commit 76127d984011a0ca55d9c939f5f9702b02ae2fe2.

--- deep_sequencing_unit/source/java/ch/ethz/bsse/cisd/dsu/tracking/main/TrackingClient.java
+++ deep_sequencing_unit/source/java/ch/ethz/bsse/cisd/dsu/tracking/main/TrackingClient.java
@@ -34,7 +34,7 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.time.DateUtils;
+import org.apache.commons.lang.time.DateUtils;
 
 import ch.ethz.bsse.cisd.dsu.tracking.email.EntityTrackingEmailGenerator;
 import ch.ethz.bsse.cisd.dsu.tracking.email.IEntityTrackingEmailGenerator;
@@ -278,10 +278,9 @@ public class TrackingClient
     }
     
     private static IApplicationServerApi initV3(Parameters params) {
-        int TIMEOUT = 30000;
         v3 = HttpInvokerUtils
                 .createServiceStub(IApplicationServerApi.class, params.getOpenbisServerURL()
-                        + IApplicationServerApi.SERVICE_URL, TIMEOUT);
+                        + IApplicationServerApi.SERVICE_URL, 10000);
         return v3;
    }
 }
