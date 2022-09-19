9300c764bc69affd073bae8d57592da2d901454a
pkupczyk <piotr.kupczyk@id.ethz.ch>
2022-09-19 10:05:13 +0200

    Merge branch '20.10.x-pat' into 20.10.5-EA

--- js-test/servers/common/core-plugins/tests/1/as/webapps/openbis-v3-api-test/html/test/test-jsVSjava.js
+++ js-test/servers/common/core-plugins/tests/1/as/webapps/openbis-v3-api-test/html/test/test-jsVSjava.js
@@ -27,6 +27,7 @@ define(["jquery", "underscore", "openbis", "test/common"], function ($, _, openb
             UnauthorizedObjectAccessException: "Java class ignored: ",
             UnsupportedObjectIdException: "Java class ignored: ",
             IApplicationServerApi: "Java class ignored: ",
+            IOperationListener: "Java class ignored: ",
             DataSetCreation: "Java class ignored: ",
             DataSetFileDownloadInputStream: "Java class ignored: ",
             IDataStoreServerApi: "Java class ignored: ",
@@ -135,7 +136,15 @@ define(["jquery", "underscore", "openbis", "test/common"], function ($, _, openb
                     var jsField = jsTypeDescription[javaField.name]
                     if (jsField) {
                         var javaFieldType = getSimpleClassName(javaField.type)
-                        var jsFieldType = _.isObject(jsField) ? jsField.name : jsField
+                        var jsFieldType = null;
+
+                        if(_.isFunction(jsField)){
+                            jsFieldType = jsField();
+                        }else if(_.isObject(jsField)){
+                            jsFieldType = jsField.name
+                        }else{
+                            jsFieldType = jsField
+                        }
 
                         if (javaFieldType !== jsFieldType) {
                             var errorResult =
