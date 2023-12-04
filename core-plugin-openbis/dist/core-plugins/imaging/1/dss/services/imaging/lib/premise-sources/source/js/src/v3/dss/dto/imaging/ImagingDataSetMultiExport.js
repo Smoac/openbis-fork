453152cb60edd149f23a6d426ba5685d61cd29d2
alaskowski <alaskowski@ethz.ch>
2023-12-04 13:33:37 +0100

    BIS-713: Moved premise backend into a new services structure; Moved DTOs to api-openbis-java project

---
+++ core-plugin-openbis/dist/core-plugins/imaging/1/dss/services/imaging/lib/premise-sources/source/js/src/v3/dss/dto/imaging/ImagingDataSetMultiExport.js
@@ -0,0 +1,51 @@
+define([ "stjs", "util/Exceptions" ], function(stjs, exceptions) {
+	var ImagingDataSetExport = function() {
+	};
+	stjs.extend(ImagingDataSetExport, null, [], function(constructor, prototype) {
+		prototype['@type'] = 'dss.dto.imaging.ImagingDataSetMultiExport';
+		constructor.serialVersionUID = 1;
+		prototype.permId = null;
+        prototype.index = null;
+		prototype.config = null;
+		prototype.metaData = null;
+
+        prototype.getPermId = function() {
+			return this.permId;
+		};
+		prototype.setPermId = function(permId) {
+			this.permId = permId;
+		};
+		prototype.getIndex = function() {
+            return this.index;
+        };
+        prototype.setIndex = function(index) {
+            this.index = index;
+        };
+		prototype.getConfig = function() {
+			return this.config;
+		};
+		prototype.setConfig = function(config) {
+			this.config = config;
+		};
+		prototype.getMetaData = function() {
+			return this.metaData;
+		};
+		prototype.setMetaData = function(metaData) {
+			this.metaData = metaData;
+		};
+		prototype.toString = function() {
+            return "ImagingDataSetExport: " + this.config;
+        };
+
+	}, {
+		config : {
+            name : "Map",
+            arguments : [ "String", "Serializable" ]
+        },
+        metaData : {
+            name : "Map",
+            arguments : [ "String", "Serializable" ]
+        }
+	});
+	return ImagingDataSetExport;
+})
