fffdb5c6919ebcfb9af41939abb2abde0d4406fd
alaskowski <alaskowski@ethz.ch>
2023-12-04 16:44:54 +0100

    BIS-713: Moved imaging js dtos to api-openbis-javascript

---
+++ api-openbis-javascript/src/v3/imaging/dto/ImagingDataSetPreview.js
@@ -0,0 +1,79 @@
+define([ "stjs", "util/Exceptions" ], function(stjs, exceptions) {
+	var ImagingDataSetPreview = function() {
+	};
+	stjs.extend(ImagingDataSetPreview, null, [], function(constructor, prototype) {
+		prototype['@type'] = 'imaging.dto.ImagingDataSetPreview';
+		constructor.serialVersionUID = 1;
+		prototype.config = null;
+		prototype.format = null;
+		prototype.bytes = null;
+		prototype.show = null;
+		prototype.width = null;
+		prototype.height = null;
+		prototype.index = null;
+		prototype.metaData = null;
+
+		prototype.getConfig = function() {
+			return this.config;
+		};
+		prototype.setConfig = function(config) {
+			this.config = config;
+		};
+		prototype.getFormat = function() {
+			return this.format;
+		};
+		prototype.setFormat = function(format) {
+			this.format = format;
+		};
+		prototype.getBytes = function() {
+			return this.bytes;
+		};
+		prototype.setBytes = function(bytes) {
+			this.bytes = bytes;
+		};
+		prototype.isShow = function() {
+			return this.show;
+		};
+		prototype.setShow = function(show) {
+			this.show = show;
+		};
+		prototype.getWidth = function() {
+            return this.bytes;
+        };
+        prototype.setWidth = function(width) {
+            this.width = width;
+        };
+        prototype.getHeight = function() {
+            return this.height;
+        };
+        prototype.setHeight = function(height) {
+            this.height = height;
+        };
+        prototype.getIndex = function() {
+            return this.index;
+        };
+        prototype.setIndex = function(index) {
+            this.index = index;
+        };
+		prototype.getMetaData = function() {
+			return this.metaData;
+		};
+		prototype.setMetaData = function(metaData) {
+			this.metaData = metaData;
+		};
+		prototype.toString = function() {
+            return "ImagingDataSetPreview: " + this.config;
+        };
+
+	}, {
+		config : {
+            name : "Map",
+            arguments : [ "String", "Serializable" ]
+        },
+        metaData : {
+            name : "Map",
+            arguments : [ "String", "Serializable" ]
+        }
+	});
+	return ImagingDataSetPreview;
+})
