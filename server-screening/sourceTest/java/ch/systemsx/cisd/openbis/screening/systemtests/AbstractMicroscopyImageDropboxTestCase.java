5901006000e9774f440ebbaccbbad27332c5504c
alaskowski <alaskowski@ethz.ch>
2024-02-15 14:47:41 +0100

    BIS-1009: revert of last fix for screening tests

--- server-screening/sourceTest/java/ch/systemsx/cisd/openbis/screening/systemtests/AbstractMicroscopyImageDropboxTestCase.java
+++ server-screening/sourceTest/java/ch/systemsx/cisd/openbis/screening/systemtests/AbstractMicroscopyImageDropboxTestCase.java
@@ -38,7 +38,7 @@ public abstract class AbstractMicroscopyImageDropboxTestCase extends AbstractIma
     {
         commonServer = (ICommonServer) applicationContext
                 .getBean(ch.systemsx.cisd.openbis.generic.shared.ResourceNames.COMMON_SERVER);
-        sessionToken = commonServer.tryAuthenticate("test", "a").getSessionToken();
+        sessionToken = commonServer.tryAuthenticate("admin", "a").getSessionToken();
         ExperimentType experimentType = new ExperimentType();
         experimentType.setCode("MICROSCOPY_EXPERIMENT");
         registerExperimentType(commonServer, experimentType);
