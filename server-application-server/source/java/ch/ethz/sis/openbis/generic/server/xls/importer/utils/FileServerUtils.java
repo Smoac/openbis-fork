f1d1a90cca9685965b298ccfc5ec7111687f042f
juanf <juanf@ethz.ch>
2024-05-10 10:36:06 +0200

    BIS-1050: Mockup of how to access the file service, untested code

--- server-application-server/source/java/ch/ethz/sis/openbis/generic/server/xls/importer/utils/FileServerUtils.java
+++ server-application-server/source/java/ch/ethz/sis/openbis/generic/server/xls/importer/utils/FileServerUtils.java
@@ -3,17 +3,25 @@ package ch.ethz.sis.openbis.generic.server.xls.importer.utils;
 import ch.ethz.sis.openbis.generic.server.FileServiceServlet;
 import ch.systemsx.cisd.openbis.generic.server.CommonServiceProvider;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileServerUtils
 {
-    private Path getFilePath(String filePath) {
-        String repositoryPath = CommonServiceProvider.tryToGetProperty(
+    private Path getFilePath(String filePath) throws IOException
+    {
+        String repositoryPathAsString = CommonServiceProvider.tryToGetProperty(
                 FileServiceServlet.REPO_PATH_KEY);
-        String repositoryFilePath = repositoryPath + filePath;
-        return Path.of(repositoryFilePath);
+        String repositoryFilePathAsString = repositoryPathAsString + filePath;
+        Path repositoryPath = Path.of(new File(repositoryPathAsString).getCanonicalPath());
+        Path repositoryFilePath =  Path.of(new File(repositoryFilePathAsString).getCanonicalPath());
+        //Security Test that repositoryFilePath is indeed inside repositoryPath
+        if(!repositoryFilePath.startsWith(repositoryPath)) {
+            throw new IllegalArgumentException("File Path is not inside the Repository Path");
+        }
+        return repositoryFilePath;
     }
 
     public byte[] read(String filePath) throws IOException
