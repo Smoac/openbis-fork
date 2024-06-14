fa85b72ebcd64d1bee24395cb0dfd2806ff66114
Mihai Cosmin Danaila <mihaicosmin.danaila@ethz.ch>
2024-06-14 16:41:26 +0200

    BIS-1031: added custom TIMEOUT parameter, extractLabel and autosizecolumns methods

--- lib-rdf/src/main/java/ch/ethz/sis/rdf/main/Utils.java
+++ lib-rdf/src/main/java/ch/ethz/sis/rdf/main/Utils.java
@@ -5,6 +5,7 @@ import ch.ethz.sis.openbis.generic.asapi.v3.dto.importer.data.ImportData;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.importer.data.ImportFormat;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.importer.options.ImportMode;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.importer.options.ImportOptions;
+import org.apache.poi.ss.usermodel.Sheet;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -29,11 +30,26 @@ public class Utils {
         return tempFile;
     }
 
+    public static String extractLabel(String uri) {
+        int hashIndex = uri.indexOf('#');
+        if (hashIndex != -1)
+        {
+            return uri.substring(hashIndex + 1);
+        } else
+        {
+            return uri;
+        }
+    }
+
+    public static void autosizeColumns(Sheet sheet, int numCols) {
+        for (int i = 0; i < numCols; i++) {
+            sheet.autoSizeColumn(i);
+        }
+    }
+
     public static void connectAndExport(String openbisASURL, String openBISDSSURL, String username, String password, Path tempFile) {
 
-        String serviceURL = openBISDSSURL == null ? openbisASURL + "/openbis/openbis" : openbisASURL;
-
-        OpenBIS openBIS = openBISDSSURL == null ? new OpenBIS(openbisASURL, TIMEOUT) : new OpenBIS(openbisASURL, openBISDSSURL, null);
+        OpenBIS openBIS = openBISDSSURL == null ? new OpenBIS(openbisASURL, TIMEOUT) : new OpenBIS(openbisASURL, openBISDSSURL, null, TIMEOUT);
 
         // 4. Call openBIS V3 API executeImport with the Excel file
         //IApplicationServerApi v3 = HttpInvokerUtils.createServiceStub(IApplicationServerApi.class, serviceURL + IApplicationServerApi.SERVICE_URL, TIMEOUT);
