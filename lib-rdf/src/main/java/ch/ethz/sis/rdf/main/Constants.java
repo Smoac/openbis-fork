5a5550e3e45175d8035730e0e69ef242ccd3ed35
Mihai Cosmin Danaila <mihaicosmin.danaila@ethz.ch>
2024-07-11 10:36:23 +0200

    BIS-1031: added default prj code

--- lib-rdf/src/main/java/ch/ethz/sis/rdf/main/Constants.java
+++ lib-rdf/src/main/java/ch/ethz/sis/rdf/main/Constants.java
@@ -8,4 +8,5 @@ public class Constants {
     public static final String SHEET_TITLE_OBJS = "Objects";
 
     public static final String SEP = "/";
+    public static final String DEFAULT_PRJ = "/DEFAULT/DEFAULT";
 }
