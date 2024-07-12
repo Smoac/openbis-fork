a82a857c890a5528d55591064dc488077fdd6160
Mihai Cosmin Danaila <mihaicosmin.danaila@ethz.ch>
2024-07-12 15:33:41 +0200

    BIS-1031: created sample and property type classes

---
+++ lib-rdf/src/main/java/ch/ethz/sis/rdf/main/entity/PropertyType.java
@@ -0,0 +1,14 @@
+package ch.ethz.sis.rdf.main.entity;
+
+import java.util.List;
+
+public class PropertyType {
+    public String code;
+    public String propertyLabel;
+    public String dataType;
+    public String vocabularyCode;
+    public String description;
+    public String metadata;
+    public String ontologyAnnotationId;
+    public boolean multiValued;
+}
---
+++ lib-rdf/src/main/java/ch/ethz/sis/rdf/main/entity/SampleType.java
@@ -0,0 +1,11 @@
+package ch.ethz.sis.rdf.main.entity;
+
+import java.util.List;
+import java.util.Map;
+
+public class SampleType {
+    public String code;
+    public String description;
+    public String ontologyAnnotationId;
+    public Map<String, String> metadata;
+}
