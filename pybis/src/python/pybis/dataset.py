9b965b90e4502ebb5a3bb0f9718603ca370a3d05
alaskowski <alaskowski@ethz.ch>
2024-07-30 14:28:19 +0200

    BIS-1394: trying to fix jenkins tests

--- pybis/src/python/pybis/dataset.py
+++ pybis/src/python/pybis/dataset.py
@@ -694,12 +694,16 @@ class DataSet(
         """
 
         if self.is_new:
+            print(f'||> IS NEW:{self.files}')
             return self.files
         else:
             fl = self.get_dataset_files().df
             print(f'||> GET DATASET FILES1:{fl}')
+            print(f'||> GET DATASET FILESS1:{fl.to_string()}')
+            print(f'||> GET DATASET FILESS2:{fl.to_markdown()}')
             print(f'||> GET DATASET FILES2:{fl[fl["directory"] == False]}')
-            print(f'||> GET DATASET FILES2:{fl[fl["directory"] == False]["path"]}')
+            print(f'||> GET DATASET FILES3:{fl[fl["directory"] == False]["path"]}')
+            print(f'||> GET DATASET FILES3:{fl[fl["directory"] == False]["path"].to_list()}')
             return fl[fl["directory"] == False]["path"].to_list()
 
     @property
