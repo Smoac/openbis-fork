861e29f1c00a6c0cb1b4b39f8bafc55f90782e6b
alaskowski <alaskowski@ethz.ch>
2024-07-30 15:35:30 +0200

    BIS-1394: fixing pybis http connection error in jenkins

--- pybis/src/python/pybis/pybis.py
+++ pybis/src/python/pybis/pybis.py
@@ -1287,6 +1287,7 @@ class Openbis:
                 "Certificate validation failed. Use o=Openbis(url, verify_certificates=False) if you are using self-signed certificates."
             ) from exc
         except requests.ConnectionError as exc:
+            print(f'||> ERROR: {exc}')
             raise requests.ConnectionError(
                 "Could not connecto to the openBIS server. Please check your internet connection, the specified hostname and port."
             ) from exc
