0180435fc2bd6b183df0a31828776a9f93e780cc
pkupczyk <pkupczyk@ethz.ch>
2024-08-14 14:26:38 +0200

    BIS-1462 : Publish the openBIS ESM bundle (and the TS type declarations) on npm

---
+++ api-openbis-javascript/npm/test-app-ts/src/App.tsx
@@ -0,0 +1,19 @@
+import openbis from 'openbis.esm';
+
+function App() {
+  async function searchSpaces() {
+    var facade = new openbis.openbis("http://localhost:8888/openbis/openbis/rmi-application-server-v3.json");
+    await facade.login("admin", "password")
+    var spaces = await facade.searchSpaces(new openbis.SpaceSearchCriteria(), new openbis.SpaceFetchOptions())
+    alert(JSON.stringify(spaces.getObjects().map(space => space.getCode())))
+  }
+  return (
+    <div>
+      openBIS bundle test
+      <br />
+      <button onClick={searchSpaces}>Search Spaces</button>
+    </div>
+  );
+}
+
+export default App;
