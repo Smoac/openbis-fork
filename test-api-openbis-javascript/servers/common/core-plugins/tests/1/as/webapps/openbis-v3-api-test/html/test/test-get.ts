9009d02e48f425c69c41c41f103346b67296915d
alaskowski <alaskowski@ethz.ch>
2024-10-15 16:59:50 +0200

    BIS-1515: Updated version of API to 3.7

--- test-api-openbis-javascript/servers/common/core-plugins/tests/1/as/webapps/openbis-v3-api-test/html/test/test-get.ts
+++ test-api-openbis-javascript/servers/common/core-plugins/tests/1/as/webapps/openbis-v3-api-test/html/test/test-get.ts
@@ -878,7 +878,7 @@ exports.default = new Promise((resolve) => {
                     .then(function () {
                         return facade.getServerInformation().then(function (serverInformation) {
                             c.assertTrue(serverInformation != null)
-                            c.assertEqual(serverInformation["api-version"], "3.6", "api-version")
+                            c.assertEqual(serverInformation["api-version"], "3.7", "api-version")
                             c.assertEqual(serverInformation["project-samples-enabled"], "true", "project-samples-enabled")
                             c.finish()
                         })
