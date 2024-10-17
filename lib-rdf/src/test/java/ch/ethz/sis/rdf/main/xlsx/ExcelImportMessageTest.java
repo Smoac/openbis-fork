b6797d851d30b187b728ad5b755c196ca63c6bd8
Andreas Meier <andreas.meier@ethz.ch>
2024-10-17 08:50:04 +0200

    Remove dead code

--- lib-rdf/src/test/java/ch/ethz/sis/rdf/main/xlsx/ExcelImportMessageTest.java
+++ lib-rdf/src/test/java/ch/ethz/sis/rdf/main/xlsx/ExcelImportMessageTest.java
@@ -8,28 +8,26 @@ public class ExcelImportMessageTest extends TestCase
 
     public void testFrom()
     {
-        String exceptionMsg = "Exception importing data: sheet: 1 line: 209 message: Invalid terms:Invalid terms:\n" +
-                "Given code 'ORTHOPAEDICSURGERYANDTRAUMATOLOGYOFTHELOCOMOTORAPPARATUS' is either too short (minimal length: 1 character) or too long (maximal length: 50 characters). (Context: []) (Context: [])";
-        UserFailureException userFailureException =new UserFailureException(exceptionMsg);
+        String exceptionMsg =
+                "Exception importing data: sheet: 1 line: 209 message: Invalid terms:Invalid terms:\n" +
+                        "Given code 'ORTHOPAEDICSURGERYANDTRAUMATOLOGYOFTHELOCOMOTORAPPARATUS' is either too short (minimal length: 1 character) or too long (maximal length: 50 characters). (Context: []) (Context: [])";
+        UserFailureException userFailureException = new UserFailureException(exceptionMsg);
         ExcelImportMessage result = ExcelImportMessage.from(userFailureException);
         assertEquals(209, result.getLine());
         assertEquals(1, result.getSheet());
-        var oracleString = "Invalid terms:Invalid terms:\n" + "Given code 'ORTHOPAEDICSURGERYANDTRAUMATOLOGYOFTHELOCOMOTORAPPARATUS' is either too short (minimal length: 1 character) or too long (maximal length: 50 characters). (Context: []) (Context: [])";
 
-
-        assertTrue(result.getMessage().contains("Given code 'ORTHOPAEDICSURGERYANDTRAUMATOLOGYOFTHELOCOMOTORAPPARATUS' is either too short (minimal length:  character) or too long (maximal length: 50 characters)."));
-
+        String oracleString =
+                "Given code 'ORTHOPAEDICSURGERYANDTRAUMATOLOGYOFTHELOCOMOTORAPPARATUS' is either too short (minimal length:  character) or too long (maximal length: 50 characters).";
+        assertTrue(result.getMessage().contains(oracleString));
 
     }
-
 
     public void testFromWithError()
     {
         String exceptionMsg = "Something else!";
-        UserFailureException userFailureException =new UserFailureException(exceptionMsg);
+        UserFailureException userFailureException = new UserFailureException(exceptionMsg);
         ExcelImportMessage result = ExcelImportMessage.from(userFailureException);
         assertNull(result);
-
 
     }
 }
