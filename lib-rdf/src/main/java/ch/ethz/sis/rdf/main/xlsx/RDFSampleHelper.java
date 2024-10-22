6fd0277418e627703f3dcef69019d2cbe3621632
Andreas Meier <andreas.meier@ethz.ch>
2024-10-22 13:40:04 +0200

    SharedIdentifier: Write literals by matching w3 URIs to xsd

--- lib-rdf/src/main/java/ch/ethz/sis/rdf/main/xlsx/RDFSampleHelper.java
+++ lib-rdf/src/main/java/ch/ethz/sis/rdf/main/xlsx/RDFSampleHelper.java
@@ -8,9 +8,19 @@ import org.apache.jena.datatypes.DatatypeFormatException;
 import org.apache.jena.datatypes.xsd.XSDDatatype;
 import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.ResourceFactory;
-import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 
-import java.util.*;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 import java.util.stream.Stream;
 
 public class RDFSampleHelper
@@ -122,16 +132,27 @@ public class RDFSampleHelper
 
                         Literal literal = ResourceFactory.createTypedLiteral(lexicalValue);
 
-                        if (XSDDatatype.XSDdateTime.getURI().equals(datatypeURI)) {
+                        if (matchUris(XSDDatatype.XSDdateTime.getURI(), datatypeURI)) {
                             //Date date = (Date) literal.getValue();
                             //System.out.println("----- DATE: " + date);
-                            propertyRowValues.createCell(idx).setCellValue((Date) literal.getValue());
-                        } else if (XSDDatatype.XSDdouble.getURI().equals(datatypeURI)) {
-                            propertyRowValues.createCell(idx).setCellValue((Double) literal.getValue());
-                        } else if (XSDDatatype.XSDint.getURI().equals(datatypeURI)) {
-                            propertyRowValues.createCell(idx).setCellValue((int) literal.getValue());
-                        } else if (XSDDatatype.XSDboolean.getURI().equals(datatypeURI)) {
-                            propertyRowValues.createCell(idx).setCellValue((boolean) literal.getValue());
+                            TemporalAccessor ta = DateTimeFormatter.ISO_INSTANT.parse(literal.getValue().toString().replaceAll("\"", ""));
+                            Instant i = Instant.from(ta);
+                            Date d = Date.from(i);
+                            propertyRowValues.createCell(idx).setCellValue(d);
+                        } else if (matchUris(XSDDatatype.XSDdouble.getURI(), datatypeURI)) {
+                            String a = literal.getValue().toString().replaceAll("\"", "");
+                            double myDouble = Double.parseDouble(a);
+                            propertyRowValues.createCell(idx).setCellValue(myDouble);
+                        } else if (matchUris(XSDDatatype.XSDint.getURI(), datatypeURI)) {
+                            String a = literal.getValue().toString().replaceAll("\"", "");
+                            int myInt = Integer.parseInt(a);
+                            propertyRowValues.createCell(idx).setCellValue(myInt);
+                        } else if (matchUris(XSDDatatype.XSDboolean.getURI(), datatypeURI)) {
+                            String a = literal.getValue().toString().replaceAll("\"", "");
+                            boolean myBool = Boolean.parseBoolean(a);
+                            propertyRowValues.createCell(idx).setCellValue(myBool);
+                        } else if (matchUris(XSDDatatype.XSDanyURI.getURI(), datatypeURI)){
+                            propertyRowValues.createCell(idx).setCellValue(literal.getString());
                         }
                     }
                 }
@@ -139,6 +160,17 @@ public class RDFSampleHelper
         }
 
         return rowNum + 1;  // Move to the next row for future entries
+    }
+
+
+    private boolean matchUris(String schemaUri, String datatypeUri){
+        if (schemaUri.equals(datatypeUri)){
+            return true;
+        }
+        return schemaUri.replace("http://www.w3.org/2001/XMLSchema#", "xsd:").equals(datatypeUri);
+
+
+
     }
 
     public void convertRDFLiteral(String rdfLiteral, Row propertyRowValues, int idx)
