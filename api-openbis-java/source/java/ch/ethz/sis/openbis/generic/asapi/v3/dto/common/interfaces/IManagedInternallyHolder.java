b1917130be3778362364c32a06dd1a416004e70e
alaskowski <alaskowski@ethz.ch>
2024-10-31 11:29:42 +0100

    BIS-1630: Fixing incorrect annotation

--- api-openbis-java/source/java/ch/ethz/sis/openbis/generic/asapi/v3/dto/common/interfaces/IManagedInternallyHolder.java
+++ api-openbis-java/source/java/ch/ethz/sis/openbis/generic/asapi/v3/dto/common/interfaces/IManagedInternallyHolder.java
@@ -2,7 +2,7 @@ package ch.ethz.sis.openbis.generic.asapi.v3.dto.common.interfaces;
 
 import ch.systemsx.cisd.base.annotation.JsonObject;
 
-@JsonObject("as.dto.common.interfaces.ICodeHolder")
+@JsonObject("as.dto.common.interfaces.IManagedInternallyHolder")
 public interface IManagedInternallyHolder {
     Boolean isManagedInternally();
 }
