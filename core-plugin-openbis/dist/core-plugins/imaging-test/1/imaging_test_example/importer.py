29c1d03a1695c0beecdd12c36f7a41d4156ecc4f
Mihai Cosmin Danaila <mihaicosmin.danaila@ethz.ch>
2024-11-07 14:20:30 +0100

    BIS-1627: removed $ in importer.py imaging

--- core-plugin-openbis/dist/core-plugins/imaging-test/1/imaging_test_example/importer.py
+++ core-plugin-openbis/dist/core-plugins/imaging-test/1/imaging_test_example/importer.py
@@ -75,9 +75,9 @@ for file in files:
     f = open(file_path, 'r')
     props = {
         'imaging_data_config': f.read(),
-        'default_dataset_view': '$IMAGING_DATASET_VIEWER'
+        'default_dataset_view': 'IMAGING_DATASET_VIEWER'
     }
-    data_set = o.new_dataset('$IMAGING_DATA',
+    data_set = o.new_dataset('IMAGING_DATA',
                              experiment='/IMAGING/TEST/TEST_COLLECTION',
                              sample='/IMAGING/TEST/TEMPLATE-TEST',
                              files=file_path,
