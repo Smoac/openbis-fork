315e787d48d23fc87ed2683706a0faa69f986176
Mihai Cosmin Danaila <mihaicosmin.danaila@ethz.ch>
2024-11-07 14:21:52 +0100

    BIS-1627: added ImageSection component

---
+++ ui-admin/src/js/components/common/imaging/components/viewer/ImageSection.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Grid2 } from "@mui/material";
+import PaperBox from "@src/js/components/common/imaging/components/common/PaperBox.js";
+import Export from "@src/js/components/common/imaging/components/viewer/Exporter.jsx";
+import constants from "@src/js/components/common/imaging/constants.js";
+import ImageListItemSection
+	from "@src/js/components/common/imaging/components/common/ImageListItemSection.js";
+import messages from '@src/js/common/messages.js'
+
+
+const ImageSection = ({ images, activeImageIdx, configExports, onActiveItemChange, handleExport }) => {
+	return (<PaperBox>
+		<Grid2 container direction='row' spacing={1}>
+			<Grid2 item xs={9} sm={10}>
+				<ImageListItemSection title={messages.get(messages.IMAGES)}
+					cols={3} rowHeight={150}
+					type={constants.IMAGE_TYPE}
+					items={images}
+					activeImageIdx={activeImageIdx}
+					onActiveItemChange={onActiveItemChange} />
+			</Grid2>
+			<Grid2 item xs={3} sm={2} container direction='column'
+				sx={{
+					justifyContent: "space-around"
+				}}>
+				{configExports.length > 0 ?
+					<Export handleExport={handleExport}
+						config={configExports} /> : <></>}
+			</Grid2>
+		</Grid2>
+	</PaperBox>);
+}
+
+export default ImageSection;
